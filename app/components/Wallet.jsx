608041746a5ec558a1ef4bfefdd8ba066c31ca92
Dharmik79 <ddholariya199@gmail.com>
2024-01-20 05:13:09 -0600

    added a seprate component for the wallet and refactor the code

---
+++ app/components/Wallet.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { WagmiConfig, createConfig } from "wagmi";
+import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
+import ConnectedWallet from "./components/ConnectedWallet";
+import { mainnet, polygon, optimism, arbitrum ,sepolia} from "wagmi/chains";
+const chains = [mainnet, polygon, optimism, arbitrum,sepolia];
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: process.env.ALCHEMY_ID, // or infuraId
+    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID || "",
+
+    // Required
+    appName: "Your App Name",
+
+    // Optional
+    appDescription: "Your App Description",
+    appUrl: "https://family.co", // your app's url
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  chains
+  }),
+);
+
+const Wallet = () => {
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+      <ConnectKitButton />
+       <ConnectedWallet />
+    
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
+};
+
+export default Wallet;
+
