b3b98d6fbf9c3743ffb193ed011560b9298122e7
Dharmik79 <ddholariya199@gmail.com>
2024-01-20 05:20:53 -0600

    added the navbar for the menu and check for the details

--- app/components/Wallet.tsx
+++ app/components/Wallet.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { WagmiConfig, createConfig } from "wagmi";
-import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
-import ConnectedWallet from "./components/ConnectedWallet";
-import { mainnet, polygon, optimism, arbitrum ,sepolia} from "wagmi/chains";
-const chains = [mainnet, polygon, optimism, arbitrum,sepolia];
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultConfig,
+} from "connectkit";
+import ConnectedWallet from "./ConnectedWallet";
+import { mainnet, polygon, optimism, arbitrum, sepolia } from "wagmi/chains";
+const chains = [mainnet, polygon, optimism, arbitrum, sepolia];
 
 const config = createConfig(
   getDefaultConfig({
@@ -19,21 +23,16 @@ const config = createConfig(
     appDescription: "Your App Description",
     appUrl: "https://family.co", // your app's url
     appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
-  chains
-  }),
+    chains,
+  })
 );
 
-const Wallet = () => {
+const Wallet = ({ children }: { children: React.ReactNode }) => {
   return (
     <WagmiConfig config={config}>
-      <ConnectKitProvider>
-      <ConnectKitButton />
-       <ConnectedWallet />
-    
-      </ConnectKitProvider>
+      <ConnectKitProvider>{children}</ConnectKitProvider>
     </WagmiConfig>
   );
 };
 
 export default Wallet;
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,8 @@
 "use client";
 
-import { WagmiConfig, createConfig } from "wagmi";
-import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
-import ConnectedWallet from "./components/ConnectedWallet";
-import { mainnet, polygon, optimism, arbitrum ,sepolia} from "wagmi/chains";
-const chains = [mainnet, polygon, optimism, arbitrum,sepolia];
-
-const config = createConfig(
-  getDefaultConfig({
-    // Required API Keys
-    alchemyId: process.env.ALCHEMY_ID, // or infuraId
-    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID || "",
-
-    // Required
-    appName: "Your App Name",
-
-    // Optional
-    appDescription: "Your App Description",
-    appUrl: "https://family.co", // your app's url
-    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
-  chains
-  }),
-);
-
 const App = () => {
   return (
-    <WagmiConfig config={config}>
-      <ConnectKitProvider>
-      <ConnectKitButton />
-       <ConnectedWallet />
-    
-      </ConnectKitProvider>
-    </WagmiConfig>
+   <div></div>
   );
 };
 
