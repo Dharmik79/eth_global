068a99b014e40bb41262f5218fc7c2446cd5b271
Dharmik79 <ddholariya199@gmail.com>
2024-01-20 22:41:08 -0600

    added the function to move to the another page and make it secure

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,62 +1,61 @@
 "use client";
 
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { ConnectKitButton } from "connectkit";
-import { useDispatch, useSelector } from 'react-redux';
-import { useAccount } from 'wagmi';
-import { setAddress,setConnected } from '@/lib/reducers/connectionSlice';
-import { RootState } from '../../lib/store';
-const navItems = [
+import { useDispatch, useSelector } from "react-redux";
+import { useAccount } from "wagmi";
+import { setAddress, setConnected } from "@/lib/reducers/connectionSlice";
+import { RootState } from "../../lib/store";
+const navSecureItems = [
+
   {
-    path: "/",
-    name: "Home",
+    path: "/event/create",
+    name: "Create Event",
   },
-  {
-    path:"/event/create",
-    name:"Create Event"
-  }
 ];
 
-
 const spinnerStyle = {
-    border: "4px solid rgba(255, 255, 255, 0.3)",
-    borderRadius: "50%",
-    borderTop: "4px solid #000",
-    width: "40px",
-    height: "40px",
-    animation: "spin 2s linear infinite",
-  };
-  
-  const spinnerContainerStyle = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh", // Adjust as needed
-  };
-  
-  // Keyframes for the spinner animation
-  const keyframes = `@keyframes spin {
+  border: "4px solid rgba(255, 255, 255, 0.3)",
+  borderRadius: "50%",
+  borderTop: "4px solid #000",
+  width: "40px",
+  height: "40px",
+  animation: "spin 2s linear infinite",
+};
+
+const spinnerContainerStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh", // Adjust as needed
+};
+
+// Keyframes for the spinner animation
+const keyframes = `@keyframes spin {
     0% { transform: rotate(0deg); }
     100% { transform: rotate(360deg); }
   }`;
-  
-  // Add keyframes to the document
-  const styleSheet = document.createElement("style");
-  styleSheet.type = "text/css";
-  styleSheet.innerText = keyframes;
-  document.head.appendChild(styleSheet);
+
+// Add keyframes to the document
+const styleSheet = document.createElement("style");
+styleSheet.type = "text/css";
+styleSheet.innerText = keyframes;
+document.head.appendChild(styleSheet);
 
 export default function NavBar() {
   let pathname = usePathname() || "/";
   const dispatch = useDispatch();
-  const { address, isConnecting,isConnected } = useAccount();
+  const { address, isConnecting, isConnected } = useAccount();
 
+  const reduxAddress = useSelector(
+    (state: RootState) => state.connection.address
+  ); // Update the selector to access the 'wallet' property
+  const reduxIsConnected = useSelector(
+    (state: RootState) => state.connection.isConnected
+  );
 
-const reduxAddress = useSelector((state: RootState) => state.connection.address); // Update the selector to access the 'wallet' property
-  const reduxIsConnected = useSelector((state: RootState) => state.connection.isConnected);
-console.log(reduxAddress,reduxIsConnected)
   useEffect(() => {
     dispatch(setAddress(address || null));
     dispatch(setConnected(isConnected));
@@ -64,7 +63,16 @@ console.log(reduxAddress,reduxIsConnected)
   return (
     <div className="border border-stone-800/90 p-[0.4rem] rounded-lg mb-12 sticky top-4 z-[100] bg-stone-900/80 backdrop-blur-md">
       <nav className="flex gap-20 relative justify-start w-full z-[100]  rounded-lg">
-        {navItems.map((item, index) => {
+        <Link
+          key={"/"}
+          className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in ${
+            pathname == "/" ? "text-zinc-100" : "text-zinc-400"
+          }`}
+          href={"/"}
+        >
+          <span>{"Home"}</span>
+        </Link>
+        {reduxIsConnected && navSecureItems.map((item, index) => {
           const isActive = item.path === pathname;
 
           return (
