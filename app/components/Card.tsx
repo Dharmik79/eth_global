7bc25f33060bf9260341d7352727692ac51073e7
Bhargav Kakadiya <kakadiyabhargav7@gmail.com>
2024-01-21 05:48:59 -0800

    merge card c

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -5,13 +5,14 @@ import { ConnectKitButton, ConnectKitProvider } from "connectkit";
 import { useEffect, useRef, useState } from "react";
 
 import abiJSONContractEventTicket from "../../public/abi/contractEventTicket.json";
-import abiJSONContractEventTicketFactory from "../../public/abi/contractEventTicketFactory.json";
+import abiGHOToken from "../../public/abi/GHOToken.json";
+
 import moment from "moment";
-const contractEventTicketFactory = {
-  address: process.env
-    .NEXT_PUBLIC_EVENT_TICKET_CONTRACT_ADDRESS as `0x${string}`,
-  abi: abiJSONContractEventTicketFactory as any,
-};
+import {
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
 
 // On Click Submit check for the status of the wallet and if not connected then connect the wallet
 const Card = ({
@@ -27,16 +28,95 @@ const Card = ({
     (state: RootState) => state.connection.isConnected
   );
 
-  const handleBuyClick = () => {
+  const [args, setArgs] = useState<(string | number | bigint)[] | undefined>(
+    undefined
+  );
+
+  const [buy, setBuy] = useState(false);
+
+  // Prepare contract write with dynamic arguments
+  const { config: configApprove } = usePrepareContractWrite({
+    address: "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60",
+    abi: abiGHOToken,
+    functionName: "approve",
+    args: args,
+  });
+  const { config: configBuy } = usePrepareContractWrite({
+    address: data?.contractAddress,
+    abi: abiJSONContractEventTicket,
+    functionName: "buyTicket",
+    args: [],
+  });
+
+  const {
+    data: contractWriteData,
+    isLoading,
+    isSuccess,
+    write,
+    error,
+    isError,
+  } = useContractWrite(configApprove);
+
+  const {
+    data: contractWriteDataBuy,
+    isLoading: isLoadingBuy,
+    isSuccess: isSuccessBuy,
+    write: writeBuy,
+    error: errorBuy,
+    isError: isErrorBuy,
+  } = useContractWrite(configBuy);
+
+  const HandleBuyClick = () => {
     if (!isReduxConnected) {
       alert("Please Connect Wallet");
     } else {
-      console.log("Buy Ticket");
+      // approve the contract to spend the amount of tokens
+      const newArgs = [
+        data?.contractAddress,
+        ethers.parseEther(data?.ticketPrice.toString() as string),
+      ];
+      setArgs(newArgs);
+      // call another write once the approve is done
+      // check if approved
+      const dataTxApprove = WaitForTransactionData(contractWriteData?.hash);
+      if (dataTxApprove) {
+        setBuy(true);
+      }
+
+      // call buy ticket
+      const dataTxBuy = WaitForTransactionData(contractWriteDataBuy?.hash);
+      if (dataTxBuy) {
+        alert("Ticket Bought");
+      }
+    }
+
+    function WaitForTx(): { data: any } {
+      return useWaitForTransaction({
+        hash: contractWriteData?.hash,
+      });
+    }
+    function WaitForTxBuy(): { data: any } {
+      return useWaitForTransaction({
+        hash: contractWriteDataBuy?.hash,
+      });
     }
   };
 
-  const ticketsLeft =
-    Number(data?.totalTickets || 0) - Number(data?.ticketSold || 0);
+  useEffect(() => {
+    // Whenever args change and if write function is available, execute the contract write
+    if (write && args) {
+      write();
+    }
+  }, [write, args]);
+
+  useEffect(() => {
+    // Whenever args change and if write function is available, execute the contract write
+    if (writeBuy && buy) {
+      writeBuy();
+    }
+  }, [writeBuy, buy]);
+
+  const ticketsLeft = Number(data?.totalTickets) - Number(data?.ticketSold);
 
   return (
     <div className="max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out">
@@ -63,8 +143,7 @@ const Card = ({
             href={data?.eventURL}
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-600 hover:text-blue-800 underline"
-          >
+            className="text-blue-600 hover:text-blue-800 underline">
             Link
           </a>
           <br />
@@ -82,10 +161,9 @@ const Card = ({
                 : "bg-gray-500 hover:bg-gray-600"
             } focus:outline-none`}
             onClick={() => {
-              handleBuyClick();
+              HandleBuyClick();
             }}
-            disabled={ticketsLeft == 0}
-          >
+            disabled={ticketsLeft == 0}>
             {ticketsLeft ? "Buy Ticket" : "Sold Out"}
           </button>
         </div>
@@ -95,3 +173,15 @@ const Card = ({
 };
 
 export default Card;
+
+function WaitForTransactionData(_hash: `0x${string}` | undefined) {
+  const {
+    data: dataWaitForTransaction,
+    isError: isErrorWaitForTransaction,
+    isLoading: isLoadingWaitForTransaction,
+  } = useWaitForTransaction({
+    hash: _hash,
+  });
+
+  return dataWaitForTransaction;
+}
